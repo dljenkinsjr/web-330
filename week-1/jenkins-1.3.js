e029a64d572572b6e81c084c3c0338c10bfbfb62
Douglas Jenkins <d_jenkinsjr@yahoo.com>
2020-08-08 14:19:21 -0700

    Initial Commit

--- week-1/jenkins-1.3.js
+++ week-1/jenkins-1.3.js
@@ -3,9 +3,15 @@ Title: Assignment 1.3;
 Author: Professor Krasso ; 
 Date: 8 August 2020; 
 Modified By: Douglas Jenkins; 
-Description: Using a function to write onto a browser 
+Description: Using functions and objects
 ;===========================================*/
 
+//Header
+var header = require('../jenkins-header');
+console.log(header.display("Douglas", "Jenkins", "Exercise 1.3"));
+console.log ('\n');
+
+// function creates 3 values that will displayed later
 function cellPhone(manufacturer, model, color, price) {
 
 this.manufacturer = manufacturer;
@@ -13,14 +19,17 @@ this.model = model;
 this.color = color;
 this.price = price;
 
+// function will return the price 
 this.getPrice = function() {
    return this.price;
 };
 
+// function will return the model
 this.getModel = function() {
     return this.model;
  };
 
+// geDetails calls a function that will display a message that calls out the create this. values and created functions for the price and model.
 this.getDetails = function() {
 
 return "Manufacturer: " +  this.manufacturer + "\n" + "Model: " + this.getModel() + "\n" + "Color: " + this.color + "\n" +  "Price: " + this.getPrice();
@@ -29,6 +38,7 @@ return "Manufacturer: " +  this.manufacturer + "\n" + "Model: " + this.getModel(
 
 };
 
+// Var is created so that you can give the information to the created values that were made.
 var phone = new cellPhone("Apple", "IPhone S", "Rose Gold", "$999.99" );
 
 console.log(phone.getDetails());
--- week-1/jenkins-1.4.js
+++ week-1/jenkins-1.4.js
@@ -3,20 +3,28 @@ Title: Assignment 1.4;
 Author: Professor Krasso ; 
 Date: 8 August 2020; 
 Modified By: Douglas Jenkins; 
-Description: Using a function to write onto a browser 
+Description: Using Duck Typing A.K.A. Interfaces 
 ;===========================================*/
 
+//Header
+var header = require('../jenkins-header');
+console.log(header.display("Douglas", "Jenkins", "Exercise 1.4"));
+console.log ('\n');
+
+// Creates a function that will call the value for car 
 function Car(model){
 
     this.model = model;
 }
 
+// creates the values for jeep to be called later
 Car.prototype.start = function(){
-
+    
     console.log("Car added to the racetrack");
 
 };
 
+// creates the values for truck to be called later
 function Truck (model, year){
 
     this.model = model;
@@ -24,12 +32,14 @@ function Truck (model, year){
 
 }
 
+// creates the values for jeep to be called later
 Truck.prototype.start = function(){
 
     console.log("Truck added to the racetrack");
 
 };
 
+// creates the values for jeep to be called later
 function Jeep (model, year, color){
 
     this.model = model;
@@ -38,32 +48,35 @@ function Jeep (model, year, color){
 
 }
 
+// prototype calls out the values at once
 Jeep.prototype.start = function(){
 
     console.log("Jeep added to the racetrack");
 
 };
 
+// will create the way to push the message out 
 var racetrack = []
-
 function driverIt(vehicle) {
     vehicle.start();
 
     racetrack.push(vehicle);
 }
 
+// creates the names of the values that was created earlier
 var Impreza = new Car("Impreza");
 var Ram = new Truck("Dodge Ram, 2020");
 var Wrangler = new Jeep("Wrangler, 2017, Blue");
 
 console.log("");
-
+// drive function will display the vehicle name and output results.
 driverIt(Impreza);
 driverIt(Ram);
 driverIt(Wrangler);
 
 console.log("\n---The following vehicles have been added to the track---");
 
+// creates a loop that will output the name of the cars
 for (var x = 0;x < racetrack.length; x++){
     console.log(racetrack[x].constructor.name + ": " + racetrack[x].model)
 }
